a1e9675d1a18ee693a3f83e022f18b18c1f2158d
AdrianMendezVargas <velociraptor088@gmail.com>
2019-09-27 22:46:07 -0400

    final

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText et1;
+    private EditText et2;
+    private TextView tv1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = (EditText)findViewById(R.id.txt_num1);
+        et2 = (EditText)findViewById(R.id.txt_num2);
+        tv1 = (TextView)findViewById(R.id.txt_resultado);
+
+    }
+
+    //Metodo que realiza la suma
+
+    public void Sumar(View componente){
+        String valor1 = et1.getText().toString();
+        String valor2 = et2.getText().toString();
+
+        int numero1 = Integer.parseInt(valor1);
+        int numero2 = Integer.parseInt(valor2);
+
+        int suma = numero1 + numero2;
+
+        String resultado = String.valueOf(suma);
+        tv1.setText(resultado);
+    }
+
+}
